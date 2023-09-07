e091483dc4ca9cb4a1cc4733979ff7d68e68539d
Amal-Vishnu <144209988+Amal-Vishnu@users.noreply.github.com>
2023-09-07 13:03:29 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,37 @@
+def evaluate_expression(expression):
+    result = 0
+    current_number = None
+    current_operator = '+'
+    for char in expression:
+        if char.isdigit():
+            if current_number is None:
+                current_number = int(char)
+            else:
+                current_number = current_number * 10 + int(char)
+        elif char in ('+', '-','/','*'):
+            if current_operator == '+':
+                result += current_number
+            elif current_operator == '-':
+                result -= current_number
+            elif current_operator =='*':
+                result *=current_number
+            elif current_operator =='/':
+                result /=current_number
+            current_operator = char
+            current_number = None 
+
+            
+    if current_operator == '+':
+        result += current_number
+    elif current_operator == '-':
+        result -= current_number
+    elif current_operator =='*':
+        result *=current_number
+    elif current_operator =='/':
+        result /=current_number
+    return result
+
+
+expression = input("enter expression :")
+result = evaluate_expression(expression)
+print(result)
